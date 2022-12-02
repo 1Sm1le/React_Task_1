6df159ba0103d2c256bfb8b90df6c0f2401b392f
1Sm1le <115559048+1Sm1le@users.noreply.github.com>
2022-12-02 20:28:35 +0400

    Add files via upload

---
+++ ReactProjects-main/src/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactDOM } from "react";
+import "./style2.css"
+
+class Header extends React.Component{
+    render(){
+       return(
+            <>
+                <h1>TestProject</h1>
+            </>
+       )
+    }
+}
+
+export default Header
